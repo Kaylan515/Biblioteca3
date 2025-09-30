4b4a9b3899d37c7273ad790643f1f8b1047d74bc
Kaylan Teodoro <kaylanteodoro344@gmail.com>
2025-09-30 13:25:29 -0300

    etapa 5 função de remoção de livros

--- funcoes.py
+++ funcoes.py
@@ -16,6 +16,7 @@ def listar_livro():
 
     for linha in cursor.fetchall(): 
         return(f"ID {linha[0]}| Título: {linha[1]} | Ano: {linha[2]} | Autor: {linha[3]} | Disponivel : {linha[4]}")
+
 def update_dispon(id_livro):
     conexao = sqlite3.connect("Biblioteca.db")
     cursor = conexao.cursor()
@@ -37,3 +38,17 @@ def update_dispon(id_livro):
         print(f"Erro: Livro com ID {id_livro} não encontrado.")
     conexao.close()
 
+def remover_livro(id_livros):
+    conexao = sqlite3.connect("Biblioteca.db")
+    cursor = conexao.cursor()
+
+    cursor.execute("DELETE FROM Biblioteca WHERE id = ?", (id_livros))
+    linhas_afetadas = cursor.rowcount
+
+    conexao.commit()
+    conexao.close()
+
+    if linhas_afetadas > 0:
+        print(f"Livro com id {id_livros} removido com sucesso.")
+    else:
+        print(f"Erro: Livro com id {id_livros} não encontrado.")
